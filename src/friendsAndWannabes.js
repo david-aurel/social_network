69b847ad82fc35cc8ec1ad694a4e25bd6dc6e35a
David Durlan <david.a.durlan@gmail.com>
2020-02-07 15:41:50 +0100

    users can see their friends and people who want to be

--- src/friendsAndWannabes.js
+++ src/friendsAndWannabes.js
@@ -4,11 +4,39 @@ import * as action from "./actions";
 
 export default function UseFriendsAndWannabes() {
     const dispatch = useDispatch();
+    const friends = useSelector(
+        state =>
+            state.friendsWannabes &&
+            state.friendsWannabes.filter(user => user.accepted)
+    );
+    const wannabes = useSelector(
+        state =>
+            state.friendsWannabes &&
+            state.friendsWannabes.filter(user => !user.accepted)
+    );
+
     useEffect(() => {
         (async () => {
-            console.log("friendsAndWannabes");
             dispatch(action.receiveFriendsAndWannabes());
         })();
-    });
-    return <div>FRIENDS AND WANNABES</div>;
+    }, []);
+
+    if (!friends) {
+        return <h1>nope</h1>;
+    }
+    return (
+        <div>
+            <p>FRIENDS:</p>
+            {friends &&
+                friends.map(friend => (
+                    <div key={friend.id}>{friend.first}</div>
+                ))}
+
+            <p>WANNABES</p>
+            {wannabes &&
+                wannabes.map(wannabes => (
+                    <div key={wannabes.id}>{wannabes.first}</div>
+                ))}
+        </div>
+    );
 }
