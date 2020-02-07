9a1dd6c29201da9c921d884494ac20823751b404
David Durlan <david.a.durlan@gmail.com>
2020-02-07 13:25:07 +0100

    setup Redux boilerplate

--- src/start.js
+++ src/start.js
@@ -2,13 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Welcome from "./welcome";
 import App from "./app";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducer from "./reducers";
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(reduxPromise))
+);
 
 // if the user is logged out, we show the welcome component, otherwise, we just show the logo in the top of the other routes
 let elem;
 if (location.pathname == "/welcome") {
     elem = <Welcome />;
 } else {
-    elem = <App />;
+    elem = (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 }
 
 ReactDOM.render(elem, document.querySelector("main"));
